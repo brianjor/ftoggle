13931fb77562af019e112e1dea149ac35161a371
brianjor <22751140+brianjor@users.noreply.github.com>
2023-12-27 19:55:49 -0800

    Changed 'beforeHandle => isSignedIn' to 'derive(isSignedIn)'

--- packages/api/src/routers/projects/features/featuresRoutes.ts
+++ packages/api/src/routers/projects/features/featuresRoutes.ts
@@ -1,103 +1,110 @@
 import Elysia, { t } from 'elysia';
 import { FeaturesController } from '../../../controllers/featuresController';
 import { hooks } from '../../../hooks';
+import { isSignedIn } from '../../../hooks/isSignedInHook';
 
 const featuresController = new FeaturesController();
 
-const getFeautresRoute = new Elysia().use(hooks).get(
-  '',
-  async ({ set, params }) => {
-    const { projectId } = params;
-    const features = await featuresController.getFeatures(projectId);
-    const response = {
-      data: {
-        features,
+const getFeautresRoute = new Elysia()
+  .use(hooks)
+  .derive(isSignedIn)
+  .get(
+    '',
+    async ({ set, params }) => {
+      const { projectId } = params;
+      const features = await featuresController.getFeatures(projectId);
+      const response = {
+        data: {
+          features,
+        },
+      };
+      set.status = 200;
+      return response;
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+      }),
+      response: {
+        200: t.Object({
+          data: t.Object({
+            features: t.Array(
+              t.Object({
+                id: t.Number(),
+                name: t.String(),
+                enabled: t.Boolean(),
+                createdAt: t.Date(),
+                modifiedAt: t.Date(),
+              }),
+            ),
+          }),
+        }),
       },
-    };
-    set.status = 200;
-    return response;
-  },
-  {
-    params: t.Object({
-      projectId: t.Numeric(),
-    }),
-    response: {
-      200: t.Object({
-        data: t.Object({
-          features: t.Array(
-            t.Object({
+    },
+  );
+
+const createFeaturesRoute = new Elysia()
+  .use(hooks)
+  .derive(isSignedIn)
+  .post(
+    '',
+    async ({ set, body, params }) => {
+      const { name } = body;
+      const { projectId } = params;
+      await featuresController.addFeature(name, projectId);
+      set.status = 200;
+      return 'Successfully added feature!';
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+      }),
+      body: t.Object({
+        name: t.String(),
+      }),
+      response: {
+        200: t.String(),
+      },
+    },
+  );
+
+const getFeatureRoute = new Elysia()
+  .use(hooks)
+  .derive(isSignedIn)
+  .get(
+    '/:featureId',
+    async ({ set, params }) => {
+      const { featureId } = params;
+      const feature = await featuresController.getFeature(featureId);
+      const response = {
+        data: {
+          feature: feature[0],
+        },
+      };
+      set.status = 200;
+      return response;
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+        featureId: t.Numeric(),
+      }),
+      response: {
+        200: t.Object({
+          data: t.Object({
+            feature: t.Object({
               id: t.Number(),
               name: t.String(),
               enabled: t.Boolean(),
               createdAt: t.Date(),
               modifiedAt: t.Date(),
             }),
-          ),
-        }),
-      }),
-    },
-    beforeHandle: [({ isSignedIn }) => isSignedIn()],
-  },
-);
-
-const createFeaturesRoute = new Elysia().use(hooks).post(
-  '',
-  async ({ set, body, params }) => {
-    const { name } = body;
-    const { projectId } = params;
-    await featuresController.addFeature(name, projectId);
-    set.status = 200;
-    return 'Successfully added feature!';
-  },
-  {
-    params: t.Object({
-      projectId: t.Numeric(),
-    }),
-    body: t.Object({
-      name: t.String(),
-    }),
-    response: {
-      200: t.String(),
-    },
-    beforeHandle: [({ isSignedIn }) => isSignedIn()],
-  },
-);
-
-const getFeatureRoute = new Elysia().use(hooks).get(
-  '/:featureId',
-  async ({ set, params }) => {
-    const { featureId } = params;
-    const feature = await featuresController.getFeature(featureId);
-    const response = {
-      data: {
-        feature: feature[0],
-      },
-    };
-    set.status = 200;
-    return response;
-  },
-  {
-    params: t.Object({
-      projectId: t.Numeric(),
-      featureId: t.Numeric(),
-    }),
-    response: {
-      200: t.Object({
-        data: t.Object({
-          feature: t.Object({
-            id: t.Number(),
-            name: t.String(),
-            enabled: t.Boolean(),
-            createdAt: t.Date(),
-            modifiedAt: t.Date(),
           }),
         }),
-      }),
-      404: t.String(),
+        404: t.String(),
+      },
     },
-    beforeHandle: [({ isSignedIn }) => isSignedIn()],
-  },
-);
+  );
 
 export const featuresRoutes = new Elysia({ prefix: '/features' })
   .use(getFeautresRoute)
