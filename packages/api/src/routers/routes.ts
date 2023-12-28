118451ffa33e8b8d1364d6050f56e6d5372e22fb
brianjor <22751140+brianjor@users.noreply.github.com>
2023-12-27 16:16:59 -0800

    Moved features routes under projects/:projectId routes

--- packages/api/src/routers/routes.ts
+++ packages/api/src/routers/routes.ts
@@ -1,9 +1,5 @@
 import Elysia from 'elysia';
 import { authRouters } from './authRoutes';
-import { featuresRoutes } from './featuresRoutes';
 import { projectsRoutes } from './projects/projectsRoutes';
 
-export const routes = new Elysia()
-  .use(authRouters)
-  .use(featuresRoutes)
-  .use(projectsRoutes);
+export const routes = new Elysia().use(authRouters).use(projectsRoutes);
