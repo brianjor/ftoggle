d4b8eb3498fca86feff6a7331142ade343af54fb
brianjor <22751140+brianjor@users.noreply.github.com>
2023-12-27 23:38:02 -0800

    Added GET '/projects/:projectId' route

--- packages/api/src/routers/projects/projectsRoutes.ts
+++ packages/api/src/routers/projects/projectsRoutes.ts
@@ -1,5 +1,6 @@
 import Elysia, { t } from 'elysia';
 import { ProjectsController } from '../../controllers/projectsController';
+import { RecordDoesNotExistError } from '../../errors/dbErrors';
 import { hooks } from '../../hooks';
 import { isSignedIn } from '../../hooks/isSignedInHook';
 import { featuresRoutes } from './features/featuresRoutes';
@@ -34,6 +35,38 @@ const getProjectsRoute = new Elysia()
     },
   );
 
+const getProjectRoute = new Elysia().derive(isSignedIn).get(
+  '/:projectId',
+  async (context) => {
+    const { projectId } = context.params;
+    const project = await projectsController.getProject(projectId);
+    if (project === undefined) {
+      throw new RecordDoesNotExistError(
+        `Project with id: ${projectId} does not exist.`,
+      );
+    }
+    return {
+      data: {
+        project,
+      },
+    };
+  },
+  {
+    params: t.Object({
+      projectId: t.Numeric(),
+    }),
+    response: t.Object({
+      data: t.Object({
+        project: t.Object({
+          id: t.Number(),
+          name: t.String(),
+        }),
+      }),
+    }),
+  },
+);
+
 export const projectsRoutes = new Elysia({ prefix: '/projects' })
   .use(new Elysia({ prefix: '/:projectId' }).use(featuresRoutes))
-  .use(getProjectsRoute);
+  .use(getProjectsRoute)
+  .use(getProjectRoute);
