4bc151dcc26d877dcf9936b81725024af753f13f
brianjor <22751140+brianjor@users.noreply.github.com>
2023-12-30 16:42:12 -0800

    Added unique constrain on features name and projectId

--- packages/db/src/schema/features.ts
+++ packages/db/src/schema/features.ts
@@ -4,26 +4,33 @@ import {
   pgTable,
   serial,
   timestamp,
+  unique,
   varchar,
 } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm/relations';
 import { projects } from '.';
 import { featuresEnvironments } from './featuresEnvironments';
 
-export const features = pgTable('features', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 256 }).notNull(),
-  isEnabled: boolean('isEnabled').default(false).notNull(),
-  createdAt: timestamp('created_at', { withTimezone: true })
-    .notNull()
-    .defaultNow(),
-  modifiedAt: timestamp('modified_at', { withTimezone: true })
-    .notNull()
-    .defaultNow(),
-  projectId: integer('project_id')
-    .notNull()
-    .references(() => projects.id),
-});
+export const features = pgTable(
+  'features',
+  {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 256 }).notNull(),
+    isEnabled: boolean('isEnabled').default(false).notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+    modifiedAt: timestamp('modified_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+    projectId: integer('project_id')
+      .notNull()
+      .references(() => projects.id),
+  },
+  (t) => ({
+    unq: unique().on(t.name, t.projectId),
+  }),
+);
 
 export const featuresRelations = relations(features, ({ one, many }) => ({
   environments: many(featuresEnvironments),
