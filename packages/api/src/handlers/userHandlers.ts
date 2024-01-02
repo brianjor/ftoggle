9374aa3712e32fbf4c8df05c7c2b98bef2948446
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-01 19:55:15 -0800

    Created GET '/users/:userId' endpoint

---
+++ packages/api/src/handlers/userHandlers.ts
@@ -0,0 +1,40 @@
+import Elysia, { t } from 'elysia';
+import { UsersController } from '../controllers/usersController';
+import { UserPermission } from '../enums/permissions';
+import { hooks } from '../hooks';
+
+const usersController = new UsersController();
+
+export const userHandlers = new Elysia().use(hooks).get(
+  '',
+  async ({ params }) => {
+    const { userId } = params;
+    return {
+      data: {
+        user: await usersController.getUserById(userId),
+      },
+    };
+  },
+  {
+    params: t.Object({
+      userId: t.String(),
+    }),
+    response: {
+      200: t.Object({
+        data: t.Object({
+          user: t.Object({
+            id: t.String(),
+            username: t.String(),
+          }),
+        }),
+      }),
+      401: t.String(),
+      403: t.String(),
+      404: t.String(),
+    },
+    beforeHandle: [
+      ({ isSignedIn }) => isSignedIn(),
+      ({ hasPermissions }) => hasPermissions([UserPermission.VIEW_USERS]),
+    ],
+  },
+);
