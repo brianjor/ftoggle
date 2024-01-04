a8aa2b000be3016912c74a4263f7726beda93b17
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-03 21:34:49 -0800

    Created GET '/projects/:projectId/environments' endpoint

--- packages/api/src/handlers/environementsHandlers.ts
+++ packages/api/src/handlers/environementsHandlers.ts
@@ -5,33 +5,59 @@ import { hooks } from '../hooks';
 
 const projectsController = new ProjectsController();
 
-export const environmentsHandlers = new Elysia().use(hooks).post(
-  '',
-  async ({ params, body }) => {
-    const { projectId } = params;
-    const { environmentName } = body;
-    const project = await projectsController.getProjectById(projectId);
+export const environmentsHandlers = new Elysia()
+  .use(hooks)
+  .post(
+    '',
+    async ({ params, body }) => {
+      const { projectId } = params;
+      const { environmentName } = body;
+      const project = await projectsController.getProjectById(projectId);
 
-    const env = await projectsController.addEnvironment(
-      environmentName,
-      projectId,
-    );
+      const env = await projectsController.addEnvironment(
+        environmentName,
+        projectId,
+      );
 
-    return `Created environment: "${env.name}" for project: "${project.name}"`;
-  },
-  {
-    params: t.Object({
-      projectId: t.Numeric(),
-    }),
-    body: t.Object({
-      environmentName: t.String(),
-    }),
-    beforeHandle: [
-      ({ isSignedIn }) => isSignedIn(),
-      ({ hasProjectPermissions }) =>
-        hasProjectPermissions(projectsController, [
-          ProjectPermission.CREATE_ENVIRONMENT,
-        ]),
-    ],
-  },
-);
+      return `Created environment: "${env.name}" for project: "${project.name}"`;
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+      }),
+      body: t.Object({
+        environmentName: t.String(),
+      }),
+      beforeHandle: [
+        ({ isSignedIn }) => isSignedIn(),
+        ({ hasProjectPermissions }) =>
+          hasProjectPermissions(projectsController, [
+            ProjectPermission.CREATE_ENVIRONMENT,
+          ]),
+      ],
+    },
+  )
+  .get(
+    '',
+    async ({ params }) => {
+      const { projectId } = params;
+
+      return {
+        data: {
+          environments: await projectsController.getEnvironments(projectId),
+        },
+      };
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+      }),
+      beforeHandle: [
+        ({ isSignedIn }) => isSignedIn(),
+        ({ hasProjectPermissions }) =>
+          hasProjectPermissions(projectsController, [
+            ProjectPermission.VIEW_ENVIRONMENTS,
+          ]),
+      ],
+    },
+  );
