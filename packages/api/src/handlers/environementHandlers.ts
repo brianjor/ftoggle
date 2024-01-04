071b85eb492ebfe5c21d30da535f7238ada070ba
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-03 21:19:50 -0800

    Created GET '/projects/:projectsId/environments/:environmentId' endpoint

--- packages/api/src/handlers/environementHandlers.ts
+++ packages/api/src/handlers/environementHandlers.ts
@@ -5,29 +5,72 @@ import { hooks } from '../hooks';
 
 const projectsController = new ProjectsController();
 
-export const environmentHandlers = new Elysia().use(hooks).delete(
-  '',
-  async ({ params }) => {
-    const { projectId, environmentId } = params;
-    const project = await projectsController.getProjectById(projectId);
-    const env = await projectsController.deleteEnvironment(
-      projectId,
-      environmentId,
-    );
+export const environmentHandlers = new Elysia()
+  .use(hooks)
+  .get(
+    '',
+    async ({ params }) => {
+      const { projectId, environmentId } = params;
+      const env = await projectsController.getEnvironmentById(
+        projectId,
+        environmentId,
+      );
+      return {
+        data: {
+          environment: env,
+        },
+      };
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+        environmentId: t.Numeric(),
+      }),
+      response: {
+        200: t.Object({
+          data: t.Object({
+            environment: t.Object({
+              id: t.Number(),
+              name: t.String(),
+              createdAt: t.Date(),
+              modifiedAt: t.Date(),
+              projectId: t.Number(),
+            }),
+          }),
+        }),
+      },
+      beforeHandle: [
+        ({ isSignedIn }) => isSignedIn(),
+        ({ hasProjectPermissions }) =>
+          hasProjectPermissions(projectsController, [
+            ProjectPermission.VIEW_ENVIRONMENTS,
+          ]),
+      ],
+    },
+  )
+  .delete(
+    '',
+    async ({ params }) => {
+      const { projectId, environmentId } = params;
+      const project = await projectsController.getProjectById(projectId);
+      const env = await projectsController.deleteEnvironment(
+        projectId,
+        environmentId,
+      );
 
-    return `Deleted environment: "${env.name}" from project: "${project.name}"`;
-  },
-  {
-    params: t.Object({
-      projectId: t.Numeric(),
-      environmentId: t.Numeric(),
-    }),
-    beforeHandle: [
-      ({ isSignedIn }) => isSignedIn(),
-      ({ hasProjectPermissions }) =>
-        hasProjectPermissions(projectsController, [
-          ProjectPermission.DELETE_ENVIRONMENT,
-        ]),
-    ],
-  },
-);
+      return `Deleted environment: "${env.name}" from project: "${project.name}"`;
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+        environmentId: t.Numeric(),
+      }),
+      beforeHandle: [
+        ({ isSignedIn }) => isSignedIn(),
+        ({ hasProjectPermissions }) =>
+          hasProjectPermissions(projectsController, [
+            ProjectPermission.DELETE_ENVIRONMENT,
+          ]),
+      ],
+    },
+  );
