d3f4d0122e6146849ab1aa749e3bb2c88bef464f
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-05 22:04:12 -0800

    Added login page

--- packages/ui/src/app/app.component.ts
+++ packages/ui/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
