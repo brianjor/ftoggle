9bc0c670edfd55d86b6ff2ad97e8f1704ab4de3a
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-08 19:08:24 -0800

    Redirect user on successful login to projects page

--- packages/ui/src/app/services/login.service.ts
+++ packages/ui/src/app/services/login.service.ts
@@ -1,23 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { edenFetch } from '@elysiajs/eden';
 import type { App } from '@ftoggle/api';
 import { from } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { paths } from '../app.routes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   api = edenFetch<App>(environment.apiBaseUrl);
-  constructor(private http: HttpClient) {}
+  constructor(private router: Router) {}
 
   login(username: string, password: string) {
     from(
       this.api('/auth/login', { method: 'POST', body: { username, password } }),
     ).subscribe({
-      next: (response) =>
-        localStorage.setItem('apiToken', response.data?.accessToken ?? ''),
+      next: (response) => {
+        if (response.status === 200) {
+          console.info('Successful login');
+          localStorage.setItem('apiToken', response.data?.accessToken ?? '');
+          this.router.navigate([paths.projects]);
+        }
+      },
       error: (error) => console.error('Error logging in', error),
     });
   }
