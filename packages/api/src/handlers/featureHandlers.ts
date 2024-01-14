d9ea742e4040c00464fb7eb46bc52384fd4d44c3
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-13 18:25:24 -0800

    Created PUT '/projoects/:projectId/features/:featureId' endpoint

--- packages/api/src/handlers/featureHandlers.ts
+++ packages/api/src/handlers/featureHandlers.ts
@@ -1,43 +1,95 @@
 import Elysia, { t } from 'elysia';
 import { FeaturesController } from '../controllers/featuresController';
 import { ProjectsController } from '../controllers/projectsController';
+import { ProjectPermission } from '../enums/permissions';
 import { hooks } from '../hooks';
+import { DataContract } from '../typeboxes/common';
+import { FeaturesTableItem } from '../typeboxes/featuresTypes';
 
 const projectsController = new ProjectsController();
 const featuresController = new FeaturesController(projectsController);
 
-export const featureHandlers = new Elysia().use(hooks).get(
-  '',
-  async ({ set, params }) => {
-    const { featureId } = params;
-    const feature = await featuresController.getFeature(featureId);
-    const response = {
-      data: {
-        feature: feature[0],
-      },
-    };
-    set.status = 200;
-    return response;
-  },
-  {
-    params: t.Object({
-      projectId: t.Numeric(),
-      featureId: t.Numeric(),
-    }),
-    response: {
-      200: t.Object({
-        data: t.Object({
-          feature: t.Object({
-            id: t.Number(),
-            name: t.String(),
-            isEnabled: t.Boolean(),
-            createdAt: t.Date(),
-            modifiedAt: t.Date(),
+export const featureHandlers = new Elysia()
+  .use(hooks)
+  .get(
+    '',
+    async ({ set, params }) => {
+      const { projectId, featureId } = params;
+      const feature = await featuresController.getProjectFeatureById(
+        projectId,
+        featureId,
+      );
+      const response = {
+        data: {
+          feature,
+        },
+      };
+      set.status = 200;
+      return response;
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+        featureId: t.Numeric(),
+      }),
+      response: {
+        200: t.Object({
+          data: t.Object({
+            feature: t.Object({
+              id: t.Number(),
+              name: t.String(),
+              isEnabled: t.Boolean(),
+              createdAt: t.Date(),
+              modifiedAt: t.Date(),
+            }),
           }),
         }),
-      }),
-      404: t.String(),
+        404: t.String(),
+      },
+      beforeHandle: [({ isSignedIn }) => isSignedIn()],
     },
-    beforeHandle: [({ isSignedIn }) => isSignedIn()],
-  },
-);
+  )
+  .put(
+    '',
+    async ({ body, params }) => {
+      const { projectId, featureId } = params;
+      await projectsController.getProjectById(projectId);
+      await featuresController.getProjectFeatureById(projectId, featureId);
+
+      const updatedFeature = await featuresController.updateFeature(
+        featureId,
+        projectId,
+        {
+          name: body.name,
+        },
+      );
+
+      return {
+        data: {
+          updatedFeature,
+        },
+      };
+    },
+    {
+      params: t.Object({
+        projectId: t.Numeric(),
+        featureId: t.Numeric(),
+      }),
+      body: t.Object(
+        {
+          name: t.Optional(t.String()),
+        },
+        { additionalProperties: false },
+      ),
+      response: DataContract(
+        t.Object({
+          updatedFeature: FeaturesTableItem,
+        }),
+      ),
+      beforeHandle: [
+        ({ isSignedIn }) => isSignedIn(),
+        ({ hasProjectPermissions }) =>
+          hasProjectPermissions([ProjectPermission.EDIT_FEATURE_TOGGLE]),
+      ],
+    },
+  );
--- packages/api/src/typeboxes/featuresTypes.ts
+++ packages/api/src/typeboxes/featuresTypes.ts
@@ -1,5 +1,17 @@
 import { Static, t } from 'elysia';
 
+/** DTO of a feature from the `features` table. */
+export const FeaturesTableItem = t.Object({
+  id: t.Number(),
+  name: t.String(),
+  isEnabled: t.Boolean(),
+  createdAt: t.Date(),
+  modifiedAt: t.Date(),
+  projectId: t.Number(),
+});
+
+export type FeaturesTableItem = Static<typeof FeaturesTableItem>;
+
 export const getFeaturesItem = t.Object({
   id: t.Number(),
   name: t.String(),
