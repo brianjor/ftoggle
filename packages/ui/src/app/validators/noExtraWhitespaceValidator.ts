80ef1a282b367824728e7794fe34245d9fab7f35
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-15 15:21:02 -0800

    Created noExtraWhitespaceValidator

---
+++ packages/ui/src/app/validators/noExtraWhitespaceValidator.ts
@@ -0,0 +1,41 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export const createNoExtraWhitespaceValidator = (
+  settings: {
+    /** Don't allow leading whitespace */
+    noLeading?: boolean;
+    /** Don't allow trailing whitespace */
+    noTrailing?: boolean;
+    /** Don't allow blank */
+    noBlank?: boolean;
+    /** Don't allow any whitespace */
+    noAny?: boolean;
+  } = {
+    noLeading: false,
+    noTrailing: false,
+    noBlank: false,
+    noAny: false,
+  },
+): ValidatorFn => {
+  return (control: AbstractControl<string>): ValidationErrors | null => {
+    const value = control.value;
+
+    if (!value) {
+      return null;
+    }
+
+    const hasLeading = /^\s/.test(value);
+    const hasTrailing = /\s$/.test(value);
+    const isBlank = /^\s+$/.test(value);
+    const hasAny = /\s/.test(value);
+
+    const isValid =
+      !(settings.noLeading && hasLeading) &&
+      !(settings.noTrailing && hasTrailing) &&
+      !(settings.noBlank && isBlank) &&
+      !(settings.noAny && hasAny);
+    return !isValid
+      ? { hasExtraWhitespace: { hasLeading, hasTrailing, isBlank } }
+      : null;
+  };
+};
