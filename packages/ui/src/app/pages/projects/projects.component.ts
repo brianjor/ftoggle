c010c20736243fc248619c33af50a1b488d166fc
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-15 15:21:56 -0800

    Added 'Add Project' button and dialog

--- packages/ui/src/app/pages/projects/projects.component.ts
+++ packages/ui/src/app/pages/projects/projects.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { paths } from '../../app.routes';
+import { CreateProjectDialogComponent } from '../../components/create-project-dialog/create-project-dialog.component';
 import { ProjectsService } from '../../services/projects.service';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    CreateProjectDialogComponent,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
@@ -14,9 +23,23 @@ export class ProjectsComponent {
   protected projects = this.projectsService.projects;
   protected paths = paths;
 
-  constructor(protected projectsService: ProjectsService) {}
+  constructor(
+    protected projectsService: ProjectsService,
+    public dialog: MatDialog,
+  ) {}
 
   ngOnInit() {
+    this.getProjects();
+  }
+
+  getProjects() {
     this.projectsService.getProjects();
+  }
+
+  openCreateProjectDialog() {
+    const createProjectDialogRef = this.dialog.open(
+      CreateProjectDialogComponent,
+    );
+    createProjectDialogRef.afterClosed().subscribe(() => this.getProjects());
   }
 }
