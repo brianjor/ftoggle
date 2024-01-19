80c571b4f4432f9138bc54e5f5d3f558299753b2
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-18 21:21:43 -0800

    Updated EnvironmentsService to use ApiService

--- packages/ui/src/app/services/environments.service.ts
+++ packages/ui/src/app/services/environments.service.ts
@@ -1,30 +1,19 @@
 import { Injectable } from '@angular/core';
-import { edenFetch } from '@elysiajs/eden';
-import { App } from '@ftoggle/api';
-import { environment } from '../../environments/environment';
-import { LocalStorageService } from './local-storage.service';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EnvironmentsService {
-  api = edenFetch<App>(environment.apiBaseUrl);
-
-  constructor(private local: LocalStorageService) {}
+  constructor(private apiService: ApiService) {}
 
   async createEnvironment(projectId: number, environment: { name: string }) {
     try {
-      await this.api('/api/projects/:projectId/environments', {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${this.local.getApiToken()}`,
+      await this.apiService.api.projects[projectId].environments.post({
+        $query: {
+          projectId: String(projectId),
         },
-        params: {
-          projectId,
-        },
-        body: {
-          environmentName: environment.name,
-        },
+        environmentName: environment.name,
       });
     } catch (err) {
       console.error('Error creating feature', err);
