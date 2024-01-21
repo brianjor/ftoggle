d4e271ecde11e35232a4b7474e557a1dc15e2bb1
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-20 22:13:16 -0800

    Added logout menu item to profile in header

--- packages/ui/src/app/components/header/header.component.ts
+++ packages/ui/src/app/components/header/header.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { paths } from '../../app.routes';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule, RouterLink],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    RouterLink,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   paths = paths;
+
+  constructor(
+    private local: LocalStorageService,
+    private router: Router,
+  ) {}
+
+  handleLogout() {
+    this.local.clearApiToken();
+    this.router.navigate([paths.login]);
+  }
 }
