727c63de42a6c4f09ef9e7f5fedbbb52af72f19a
brianjor <22751140+brianjor@users.noreply.github.com>
2024-01-31 18:48:03 -0800

    Display projects in a table

--- packages/ui/src/app/pages/projects/projects.component.ts
+++ packages/ui/src/app/pages/projects/projects.component.ts
@@ -1,7 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
 import { paths } from '../../app.routes';
 import { CreateProjectDialogComponent } from '../../components/create-project-dialog/create-project-dialog.component';
@@ -11,10 +13,12 @@ import { ProjectsService } from '../../services/projects.service';
   selector: 'app-projects',
   standalone: true,
   imports: [
+    CommonModule,
     CreateProjectDialogComponent,
     RouterLink,
     MatButtonModule,
     MatIconModule,
+    MatTableModule,
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
@@ -22,6 +26,7 @@ import { ProjectsService } from '../../services/projects.service';
 export class ProjectsComponent {
   protected projects = this.projectsService.projects;
   protected paths = paths;
+  protected displayedColumns = ['name', 'createdAt'];
 
   constructor(
     protected projectsService: ProjectsService,
