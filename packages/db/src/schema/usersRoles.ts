d225117b7e1a76176b1072f9257b71bf4ab9b3d8
brianjor <22751140+brianjor@users.noreply.github.com>
2024-02-11 20:44:31 -0800

    Changed userId on usersRoles table to text

--- packages/db/src/schema/usersRoles.ts
+++ packages/db/src/schema/usersRoles.ts
@@ -1,5 +1,5 @@
 import { relations } from 'drizzle-orm';
-import { integer, pgTable, primaryKey, varchar } from 'drizzle-orm/pg-core';
+import { integer, pgTable, primaryKey, text } from 'drizzle-orm/pg-core';
 import { roles } from './roles';
 import { users } from './users';
 
@@ -9,7 +9,7 @@ export const usersRoles = pgTable(
     roleId: integer('role_id')
       .notNull()
       .references(() => roles.id),
-    userId: varchar('user_id', { length: 15 })
+    userId: text('user_id')
       .notNull()
       .references(() => users.id),
   },
