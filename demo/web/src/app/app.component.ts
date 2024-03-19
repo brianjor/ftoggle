0773e347081657c482db206007004f8e27f0c970
brianjor <22751140+brianjor@users.noreply.github.com>
2024-03-18 21:54:52 -0700

    Updated web demo to use string[]s for context fields

--- demo/web/src/app/app.component.ts
+++ demo/web/src/app/app.component.ts
@@ -13,9 +13,9 @@ import { FToggle } from '@ftoggle/clients-web';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  context = { asd: 'asd' };
+  context = { asd: ['asd'] };
   private fToggle = new FToggle({
-    apiToken: 'tp1:dev:ec8d1b5d-1b74-4fa7-a8cd-ef6a14b24bb4',
+    apiToken: 'tp1:dev:13f614c1-be4f-4c9c-95e1-e34f0389279b',
     baseUrl: 'http://localhost:8080',
     refreshInterval: 5,
     context: this.context,
@@ -27,6 +27,11 @@ export class AppComponent {
     this.fToggle.on('changed', () =>
       this.isFeatureEnabled.set(this.fToggle.isEnabled('feature')),
     );
+  }
+
+  updateAsd(event: Event) {
+    const value = (event.target as unknown as { value: string }).value;
+    this.context.asd = value.split(',').map((v) => v.trim());
   }
 
   pingApi() {
